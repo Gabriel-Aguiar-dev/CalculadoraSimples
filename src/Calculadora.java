39c2479a12902f79251339b4f383ee4af56fe997
Gabriel-Aguiar-dev <gabriel.aghot22@gmail.com>
2022-12-03 19:12:17 -0300

    add 3

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -7,6 +7,8 @@ public class Calculadora {
         int n1, n2;
         int soma, subtracao, multiplaicacao, divisao;
         int op;
+        String sair;
+
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("=======> SELECIONE UMA OPERACAO <=======");
@@ -15,6 +17,8 @@ public class Calculadora {
         System.out.println("[2] -> SUBTRAIR");
         System.out.println("[3] -> MULTIPLICAR");
         System.out.println("[4] -> DIVIDIR");
+        System.out.println("[5] -> SAIR DA CALCULADORA");
+
         System.out.println("               ");
         System.out.println(">>>>>>>>>>>> Digite sua opção: ");
 
@@ -31,14 +35,17 @@ public class Calculadora {
                 soma = n1 + n2;
                 System.out.println("O resultado da soma é: " + soma);
                 break;
+
             case 2:
                 subtracao = n1 - n2;
                 System.out.println("O resultado da subtração é: " + subtracao);
                 break;
+
             case 3:
                 multiplaicacao = n1 * n2;
                 System.out.println("O resultado dao é: " + multiplaicacao);
                 break;
+
             case 4:
                 if (n1 < n2) {
                     System.out.println("Imposivel realizar o calculo");
@@ -46,8 +53,8 @@ public class Calculadora {
                     divisao = n1 / n2;
                     System.out.println("O resultado da divisão é: " + divisao);
                     break;
-                }
 
+                }
         }
     }
 }
